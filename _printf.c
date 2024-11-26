0aa9d590e1a979409bf82a5fb6c6abaf1f7cfdd5
Franco-byte <fd.reyes3110@gmail.com>
2024-11-26 20:56:02 +0000

    Proyect_printf

--- _printf.c
+++ _printf.c
@@ -1,6 +1,57 @@
 #include "main.h"
 
 /**
+ * aux_c - Funcion que ejecuta el controlador %c
+ * @args: Utiliza la lista args
+ *
+ * Return: uno porque imprime solo un caracter para que cuente count
+ */
+
+int aux_c(va_list args)
+{
+	char c = va_arg(args, int);
+
+	write(1, &c, 1);
+	return (1);
+}
+
+/**
+ * aux_s - Funcion que ejecuta el controlador %s
+ * @args: utiliza la lista args
+ *
+ * Return: Returna la cantidad de caracteres del string
+ * almacenados en la variable count
+ */
+
+int aux_s(va_list args)
+{
+	char *s = va_arg(args, char*);
+	int count = 0, i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		write(1, &s[i], 1);
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * aux_por - Funcion que ejecuta el controlador %
+ * @args: Utiliza la lista args
+ *
+ * Return: 1 porque solo immprime un caracter %
+ */
+
+int aux_por(va_list args)
+{
+	char por = '%';
+
+	write(1, &por, 1);
+	return (1);
+}
+
+/**
  * _printf - Copia de la funcion printf
  * @format: Formato que debe imprimir y prosezar _printf
  *
@@ -18,30 +69,17 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] == '%' && format[i + 1] == 'c')
 		{
-			char c = va_arg(args, int);
-
-			write(1, &c, 1);
-			count++;
+			count += aux_c(args);
 			i++;
 		}
 		else if (format[i] == '%' && format[i + 1] == 's')
 		{
-			char *s = va_arg(args, char*);
-			int iter;
-
-			for (iter = 0; s[iter] != '\0'; iter++)
-			{
-				write(1, &s[iter], 1);
-				count++;
-			}
+			count += aux_s(args);
 			i++;
 		}
 		else if (format[i] == '%' && format[i + 1] == '%')
 		{
-			char por = va_arg(args, int);
-
-			write(1, "%", 1);
-			count++;
+			count += aux_por(args);
 			i++;
 		}
 		else
@@ -54,4 +92,3 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (count);
 }
-
