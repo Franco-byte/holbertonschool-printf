bcfbf5a33629b672b1bd86c8d39064273a1ff7b3
Franco-byte <fd.reyes3110@gmail.com>
2024-11-26 20:32:48 +0000

    Proyect_printf

--- _printf.c
+++ _printf.c
@@ -1,30 +1,57 @@
 #include "main.h"
 
 /**
+ * _printf - Copia de la funcion printf
+ * @format: Formato que debe imprimir y prosezar _printf
  *
- *
- *
+ * Return: Numeros de caracteres que tiene cada fomrato
  */
 
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i;
+	int i, count = 0;
 
 	va_start(args, format);
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		write(1, &format[i], 1);
-		switch (
+		if (format[i] == '%' && format[i + 1] == 'c')
+		{
+			char c = va_arg(args, int);
+
+			write(1, &c, 1);
+			count++;
+			i++;
+		}
+		else if (format[i] == '%' && format[i + 1] == 's')
+		{
+			char *s = va_arg(args, char*);
+			int iter;
+
+			for (iter = 0; s[iter] != '\0'; iter++)
+			{
+				write(1, &s[iter], 1);
+				count++;
+			}
+			i++;
+		}
+		else if (format[i] == '%' && format[i + 1] == '%')
+		{
+			char por = va_arg(args, int);
+
+			write(1, "%", 1);
+			count++;
+			i++;
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
 	}
 
 	va_end(args);
-	return (i);
+	return (count);
 }
 
-int main(void)
-{
-	_printf("Hola tengo 25 anos\n");
-	return (0);
-}
